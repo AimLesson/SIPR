9084886cc1cbda7b2f87d91909c2504c2896914a
Gerson Manuel Suginato <manuelsugianto@gmail.com>
2024-07-13 11:50:08 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
+        @if (Auth::user()->role == 'admin')
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard Administrator') }}
+        </h2>
+        @endif
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard User') }}
         </h2>
     </x-slot>
 
